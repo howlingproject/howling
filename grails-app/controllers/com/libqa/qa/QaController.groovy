a860891a01a3863ba1cac57f8481cc3f0b389de1
yongpwi <seeds.pear.jr@gmail.com>
2014-10-12 13:36:31 +0900

    gitignore, qacontroller 수정

--- grails-app/controllers/com/libqa/qa/QaController.groovy
+++ grails-app/controllers/com/libqa/qa/QaController.groovy
@@ -3,15 +3,18 @@ package com.libqa.qa
 class QaController {
     static layout = 'main'
 
-    def main() {
-        log.debug("Qa Main page is called!")
-    }
+    def main(){
 
-    def my() {
-        log.debug("Qa my page is called!")
     }
-
-    def view() {
-        log.debug("Qa view page is called!")
-    }
+//    def main() {
+//        log.debug("Qa Main page is called!")
+//    }
+//
+//    def my() {
+//        log.debug("Qa my page is called!")
+//    }
+//
+//    def view() {
+//        log.debug("Qa view page is called!")
+//    }
 }
