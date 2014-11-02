2f49a9ba79a7b5d619af97767a34f3c4f637fdef
yionlee <performizer@gmail.com>
2014-11-02 14:55:31 +0900

    대소문자 도메인 변경 - 이넘 추가

--- grails-app/domain/com/libqa/space/SpaceAccessUser.groovy
+++ grails-app/domain/com/libqa/space/SpaceAccessUser.groovy
@@ -4,10 +4,15 @@ class SpaceAccessUser {
 
     Long spaceId
     Long userId
-    char isDeleted // (Y,N)
+    char isDeleted = "N"// (Y,N)
     Date insertDate
     Date updateDate
 
+    static belongsTo = Space
+
     static constraints = {
+        spaceId (blank:false, nullable: false)
+        userId (blank:false, nullable: false)
+        isDeleted (nullable: false)
     }
 }
