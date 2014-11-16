7f48971293965b0f0cdf785c54c04f5f28c004d3
yionlee <performizer@gmail.com>
2014-11-16 16:22:31 +0900

    Spacd UI작업

---
+++ grails-app/services/com/libqa/space/SpaceService.groovy
@@ -0,0 +1,13 @@
+package com.libqa.space
+
+import grails.transaction.Transactional
+
+@Transactional
+class SpaceService {
+
+    def save() {
+
+    }
+
+
+}
---
+++ test/unit/com/libqa/space/SpaceServiceSpec.groovy
@@ -0,0 +1,20 @@
+package com.libqa.space
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(SpaceService)
+class SpaceServiceSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
