a25ca34df2beac4a9eccea3246505c1279cb9e42
yongpwi <seeds.pear.jr@gmail.com>
2015-01-11 13:27:00 +0900

    KeywordList 관련 파일 생성

---
+++ grails-app/controllers/com/libqa/common/KeywordListController.groovy
@@ -0,0 +1,18 @@
+package com.libqa.common
+
+class KeywordListController {
+
+    // 1. keywordlist select
+    // 2. keywordlist cnt check
+    // 3. keywordlist cnt = 0
+    //      then keywordlist insert
+    //      return keywordId
+    // 4. keywordlist cnt != 0 ==
+    //      then keywordlist cnt update
+    //      return keywordId
+    def keywordListService
+
+    def keywordCheck(params) {
+        keywordListService.keywordCheck(params)
+    }
+}
